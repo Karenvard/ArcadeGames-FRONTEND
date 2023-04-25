df1533171da078e600a862b6717e3387844fb2f3
Karen Vardanian <vkaren1777@icloud.com>
2023-04-25 23:50:29 +0200

    repair App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, {CSSProperties, useEffect} from 'react';
+import "react-toastify/dist/ReactToastify.css"
+import {ToastContainer, toast} from "react-toastify";
+import {Routes, Route, Navigate} from "react-router-dom";
+import {useAppSelector} from "./utils/hooks";
+import SignIn from "./components/pages/SignIn";
+import {styled} from "@mui/material";
+import Home from "./components/pages/Home";
+import NavBar from "./components/common/NavBar";
+import SelectMode from "./components/pages/SelectMode";
+import TicTacToe from "./components/pages/TicTacToe";
+
+const ContainerDiv = styled("div")({
+    "--back-c": "#02387d",
+    margin: "0",
+    padding: "0",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "var(--back-c)"
+})
+
 
 function App() {
-  return (
-    <div >
+    const {isAuth} = useAppSelector(state => state.authReducer);
+    return (
+        <ContainerDiv>
+            <ToastContainer/>
+            {isAuth
+                ? <>
+                    <NavBar/>
+                    <Routes>
+                        <Route path="*" element={<Navigate to="/home"/>}/>
+                        <Route path="/home" element={<Home/>}/>
+                        <Route path="/selectMode" element={<SelectMode/>}/>
+                        <Route path="/tic-tac-toe/:id" element={<TicTacToe/>}/>
+                        <Route path="/tic-tac-toe" element={<TicTacToe/>}/>
+                        <Route path="/bridge/:id" element={<h1>bridge</h1>}/>
+                    </Routes>
+                </>
 
-    </div>
-  );
+
+                : <Routes>
+                    <Route path="*" element={<Navigate to="/signin"/>}/>
+                    <Route path="/signin" element={<SignIn/>}/>
+                </Routes>
+            }
+
+        </ContainerDiv>
+    )
 }
 
 export default App;
